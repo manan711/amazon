ecf5fd80f0b32b8421282a832a7cda41d9ff52b7
Manan Patel <mananpatel.711@gmail.com>
2024-01-30 01:36:35 -0500

    Header set.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
 	return (
 		// BEM convention
 		<div className="app">
-			<h1>Let's build the Amazon Store!</h1>
-			{/* Header */}
-			{/* Home */}
+			<Header />
+			<Home />
 		</div>
 	);
 }
---
+++ src/Header.1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Header() {
+	return (
+		<div className="header">
+			<img
+				className="header__logo"
+				src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+			/>
+		</div>
+	);
+}
